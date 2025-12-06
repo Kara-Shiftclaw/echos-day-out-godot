c99b3bd0949b7219282cc5a2428bd7790c009b09
Kara-Shiftclaw <herpaderp201@gmail.com>
2025-12-05 17:36:45 -0800

    Add a title screen

---
+++ menu/scripts/title_screen.gd
@@ -0,0 +1,9 @@
+extends Node2D
+
+const FirstStage := preload("res://stages/intro_mountain.tscn")
+
+func _ready() -> void:
+	$"Options/VBoxContainer/New Game".grab_focus.call_deferred()
+
+func new_game() -> void:
+	get_tree().change_scene_to_packed(FirstStage)
