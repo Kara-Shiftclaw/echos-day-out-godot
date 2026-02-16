ee44b70c4ab3932d38ccbfdef875068106104288
Kara-Shiftclaw <herpaderp201@gmail.com>
2026-02-15 17:30:25 -0800

    Add intro cutscene with Echo

---
+++ oneoff/cutscenes/scripts/intro.gd
@@ -0,0 +1,21 @@
+extends Node2D
+
+func _ready() -> void:
+	if Global.flags.get("intro_cutscene", false):
+		queue_free()
+	else:
+		Global.echo.hide()
+		Global.health = Global.max_health
+		get_tree().paused = true
+		$CutsceneBars.start()
+		$AnimationPlayer.play("cutscene")
+
+func spawn_energy_orbs(amt: int) -> void:
+	EnergyOrb.create_n(amt, $EnergyOrbCollector.global_position, $EnergyOrbCollector)
+
+func conclude() -> void:
+	Global.flags.set("intro_cutscene", true)
+	Global.echo.show()
+	Global.echo.global_position = $CutsceneEcho.global_position
+	get_tree().paused = false
+	$CutsceneBars.end()
