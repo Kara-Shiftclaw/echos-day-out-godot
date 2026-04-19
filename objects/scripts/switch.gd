74d6ee5fea39590c5747bab8f91436894b4e3634
Kara-Shiftclaw <herpaderp201@gmail.com>
2026-04-19 13:38:08 -0700

    Add climb up Pyrite Plunge

--- objects/scripts/switch.gd
+++ objects/scripts/switch.gd
@@ -3,6 +3,7 @@ extends Area2D
 const ACTIVATED_FLAG := "activated"
 
 signal activate()
+signal already_activated()
 
 var activated := false
 
@@ -10,6 +11,7 @@ func _ready() -> void:
 	if Global.has_node_flag(self, ACTIVATED_FLAG):
 		activated = true
 		$AnimationPlayer.play("auto_activated")
+		already_activated.emit()
 
 func do_activate() -> void:
 	if activated:
