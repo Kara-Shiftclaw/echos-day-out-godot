62d53b44958529881865b2acf80fe9126dbfcbdc
Kara-Shiftclaw <herpaderp201@gmail.com>
2026-08-16 20:21:36 -0700

    Update Crush wall graphics

--- objects/scripts/painted_wall.gd
+++ objects/scripts/painted_wall.gd
@@ -2,3 +2,11 @@ extends StaticBody2D
 
 func break_wall(_other: Node2D) -> void:
 	$AnimationPlayer.play("break")
+
+func slow_smol_entered(other: Node2D) -> void:
+	if other is Player:
+		other.speed_scale = 0.5
+
+func slow_smol_exited(other: Node2D) -> void:
+	if other is Player:
+		other.speed_scale = 1.
