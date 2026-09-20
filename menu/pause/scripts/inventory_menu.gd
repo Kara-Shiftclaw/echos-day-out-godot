7069fef8e7742bcd0ddef89cea87d9da62fcd972
Kara-Shiftclaw <herpaderp201@gmail.com>
2026-09-19 17:54:21 -0700

    Formalize item translations

--- menu/pause/scripts/inventory_menu.gd
+++ menu/pause/scripts/inventory_menu.gd
@@ -3,55 +3,54 @@ extends Control
 
 const Metadata := preload("res://menu/pause/scripts/inventory_item.gd").Metadata
 const InventoryItem := preload("res://menu/pause/inventory_item.tscn")
-const FOOD := "Food"
-const PORTAL_CORE := "Portal Core"
-const ARTIFACT := "Artifact"
-const PYRITE := "Pyrite"
-const MUSH := "Mycelium Map"
-const INJECTOR := "Portal Injector"
+const FOOD := "ITEM_FOOD_NAME"
+const PORTAL_CORE := "ITEM_CORE_NAME"
+const ARTIFACT := "ITEM_ARTIFACT_NAME"
+const PYRITE := "ITEM_PYRITE_NAME"
+const MUSH := "ITEM_MUSH_NAME"
+const INJECTOR := "ITEM_INJECTOR_NAME"
 
-var used_portal_core := Metadata.new("", "Portal Core Scraps", "BROKEN_CORE", 19)
-var mush_meal := Metadata.new("has_mush_meal", "Mush-Meal", "MUSH_MEAL", 11)
-var mush_in_meal := Metadata.new("", "Mush-Meal (Occupied)", "MUSH_IN_MEAL", 12)
-var max_pyrite := Metadata.new("", "Excessive Pyrite", "MAX_PYRITE", 14)
+var used_portal_core := Metadata.new("", "ITEM_BROKEN_CORE_NAME", "ITEM_BROKEN_CORE", 19)
+var mush_meal := Metadata.new("has_mush_meal", "ITEM_MUSH_MEAL_NAME", "ITEM_MUSH_MEAL", 11)
+var mush_in_meal := Metadata.new("", "ITEM_MUSH_IN_MEAL_NAME", "ITEM_MUSH_IN_MEAL", 12)
+var max_pyrite := Metadata.new("", "ITEM_MAX_PYRITE_NAME", "ITEM_MAX_PYRITE", 14)
 static var all_items_metadata: Array[Metadata] = [
-	Metadata.new("res://stages/heftwind_hills.tscn|/root/HeftwindHills/Objects/Food|collected", FOOD, "FOOD_1", 0),
-	Metadata.new("res://stages/verdant_cavern.tscn|/root/VerdantCavern/Objects/Food|collected", FOOD, "FOOD_3", 0),
-	Metadata.new("res://stages/creaking_depths.tscn|/root/CreakingDepths/Objects/Food|collected", FOOD, "FOOD_4", 0),
-	Metadata.new("res://stages/treetop_expanse.tscn|/root/TreetopExpanse/Objects/Food|collected", FOOD, "FOOD_5", 0),
-	Metadata.new("res://res://stages/echo_peak.tscn|/root/EchoPeak/Objects/Food|collected", FOOD, "FOOD_7", 0),
-	Metadata.new("res://res://stages/billowing_heights.tscn|/root/BillowingHeights/Objects/Food|collected", FOOD, "FOOD_8", 0),
-	Metadata.new("res://stages/charred_cavern.tscn|/root/CharredCavern/Objects/Food|collected", FOOD, "FOOD_2", 0), # Actually blocked until Crush
-	Metadata.new("res://res://stages/bramble.tscn|/root/Bramble/Objects/Food|collected", FOOD, "FOOD_6", 0), # Bramble, so last
-	Metadata.new("res://stages/heftwind_hills.tscn|/root/HeftwindHills/HealthUp|collected", "Draconic Energy", "ENERGY_1", 2),
-	Metadata.new("res://stages/sporeways.tscn|/root/Sporeways/Objects/HealthUp|collected", "Draconic Energy", "ENERGY_2", 2),
-	Metadata.new("res://stages/creaking_depths.tscn|/root/CreakingDepths/Objects/HealthUp|collected", "Draconic Energy", "ENERGY_3", 2),
-	Metadata.new("res://stages/verdant_cavern.tscn|/root/VerdantCavern/Objects/HealthUp|collected", "Draconic Energy", "ENERGY_6", 2), # Was moved earlier
-	Metadata.new("res://stages/treetop_expanse.tscn|/root/TreetopExpanse/Objects/HealthUp|collected", "Draconic Energy", "ENERGY_4", 2),
-	Metadata.new("res://stages/echo_peak.tscn|/root/EchoPeak/Objects/HealthUp|collected", "Draconic Energy", "ENERGY_5", 2),
-	Metadata.new("res://stages/heftwind_hills.tscn|/root/HeftwindHills/Objects/PortalCore|collected", PORTAL_CORE, "CORE_1", 3),
-	Metadata.new("res://stages/sporeways.tscn|/root/Sporeways/Objects/PortalCore|collected", PORTAL_CORE, "CORE_3", 3),
-	Metadata.new("res://stages/charred_cavern.tscn|/root/CharredCavern/Objects/PortalCore|collected", PORTAL_CORE, "CORE_4", 3),
-	Metadata.new("res://stages/treetop_expanse.tscn|/root/TreetopExpanse/Objects/PortalCore|collected", PORTAL_CORE, "CORE_5", 3),
-	Metadata.new("res://stages/billowing_heights.tscn|/root/BillowingHeights/Objects/PortalCore|collected", PORTAL_CORE, "CORE_6", 3),
-	Metadata.new("res://stages/crater.tscn|/root/Crater/Objects/PortalCore|collected", PORTAL_CORE, "CORE_2", 3), # Moved way later
-	Metadata.new("", ARTIFACT, "ARTIFACT", 4),
-	Metadata.new("has_journal", "\"Faces of the Valley\"", "JOURNAL_1", 5),
-	Metadata.new("has_echo_journal_1", "Echo's Journal", "JOURNAL_2", 6),
-	Metadata.new("has_echo_journal_2", "------'s Journal", "JOURNAL_3", 7),
-	Metadata.new(Echo.SWORD_BLADE_FLAG, "Legendary Sword Blade", "SWORD_BLADE", 8),
-	Metadata.new(Echo.SWORD_HILT_FLAG, "Legendary Sword Hilt", "SWORD_HILT", 9),
-	Metadata.new("", MUSH, "MUSH", 10),
-	Metadata.new("pyrite", PYRITE, "PYRITE", 13),
-	Metadata.new("has_poison", "Poisoned Soup", "POISON", 15),
-	Metadata.new("has_crushed_claw", "Crushed Claw", "CRUSHED_CLAW", 16),
-	Metadata.new("has_injector", INJECTOR, "INJECTOR", 17),
-	Metadata.new("resize_injector", "Portal Injector XL", "INJECTOR_XL", 18),
+	Metadata.new("res://stages/heftwind_hills.tscn|/root/HeftwindHills/Objects/Food|collected", FOOD, "ITEM_FOOD_1", 0),
+	Metadata.new("res://stages/verdant_cavern.tscn|/root/VerdantCavern/Objects/Food|collected", FOOD, "ITEM_FOOD_3", 0),
+	Metadata.new("res://stages/creaking_depths.tscn|/root/CreakingDepths/Objects/Food|collected", FOOD, "ITEM_FOOD_4", 0),
+	Metadata.new("res://stages/treetop_expanse.tscn|/root/TreetopExpanse/Objects/Food|collected", FOOD, "ITEM_FOOD_5", 0),
+	Metadata.new("res://res://stages/echo_peak.tscn|/root/EchoPeak/Objects/Food|collected", FOOD, "ITEM_FOOD_7", 0),
+	Metadata.new("res://res://stages/billowing_heights.tscn|/root/BillowingHeights/Objects/Food|collected", FOOD, "ITEM_FOOD_8", 0),
+	Metadata.new("res://stages/charred_cavern.tscn|/root/CharredCavern/Objects/Food|collected", FOOD, "ITEM_FOOD_2", 0), # Actually blocked until Crush
+	Metadata.new("res://res://stages/bramble.tscn|/root/Bramble/Objects/Food|collected", FOOD, "ITEM_FOOD_6", 0), # Bramble, so last
+	Metadata.new("res://stages/heftwind_hills.tscn|/root/HeftwindHills/HealthUp|collected", "ITEM_ENERGY_NAME", "ITEM_ENERGY_1", 2),
+	Metadata.new("res://stages/sporeways.tscn|/root/Sporeways/Objects/HealthUp|collected", "ITEM_ENERGY_NAME", "ITEM_ENERGY_2", 2),
+	Metadata.new("res://stages/creaking_depths.tscn|/root/CreakingDepths/Objects/HealthUp|collected", "ITEM_ENERGY_NAME", "ITEM_ENERGY_3", 2),
+	Metadata.new("res://stages/verdant_cavern.tscn|/root/VerdantCavern/Objects/HealthUp|collected", "ITEM_ENERGY_NAME", "ITEM_ENERGY_6", 2), # Was moved earlier
+	Metadata.new("res://stages/treetop_expanse.tscn|/root/TreetopExpanse/Objects/HealthUp|collected", "ITEM_ENERGY_NAME", "ITEM_ENERGY_4", 2),
+	Metadata.new("res://stages/echo_peak.tscn|/root/EchoPeak/Objects/HealthUp|collected", "ITEM_ENERGY_NAME", "ITEM_ENERGY_5", 2),
+	Metadata.new("res://stages/heftwind_hills.tscn|/root/HeftwindHills/Objects/PortalCore|collected", PORTAL_CORE, "ITEM_CORE_1", 3),
+	Metadata.new("res://stages/sporeways.tscn|/root/Sporeways/Objects/PortalCore|collected", PORTAL_CORE, "ITEM_CORE_3", 3),
+	Metadata.new("res://stages/charred_cavern.tscn|/root/CharredCavern/Objects/PortalCore|collected", PORTAL_CORE, "ITEM_CORE_4", 3),
+	Metadata.new("res://stages/treetop_expanse.tscn|/root/TreetopExpanse/Objects/PortalCore|collected", PORTAL_CORE, "ITEM_CORE_5", 3),
+	Metadata.new("res://stages/billowing_heights.tscn|/root/BillowingHeights/Objects/PortalCore|collected", PORTAL_CORE, "ITEM_CORE_6", 3),
+	Metadata.new("res://stages/crater.tscn|/root/Crater/Objects/PortalCore|collected", PORTAL_CORE, "ITEM_CORE_2", 3), # Moved way later
+	Metadata.new("", ARTIFACT, "ITEM_ARTIFACT", 4),
+	Metadata.new("has_journal", "ITEM_JOURNAL_1_NAME", "ITEM_JOURNAL_1", 5),
+	Metadata.new("has_echo_journal_1", "ITEM_JOURNAL_2_NAME", "ITEM_JOURNAL_2", 6),
+	Metadata.new("has_echo_journal_2", "ITEM_JOURNAL_3_NAME", "ITEM_JOURNAL_3", 7),
+	Metadata.new(Echo.SWORD_BLADE_FLAG, "ITEM_SWORD_BLADE_NAME", "ITEM_SWORD_BLADE", 8),
+	Metadata.new(Echo.SWORD_HILT_FLAG, "ITEM_SWORD_HILT_NAME", "ITEM_SWORD_HILT", 9),
+	Metadata.new("", MUSH, "ITEM_MUSH", 10),
+	Metadata.new("pyrite", PYRITE, "ITEM_PYRITE", 13),
+	Metadata.new("has_poison", "ITEM_POISON_NAME", "ITEM_POISON", 15),
+	Metadata.new("has_crushed_claw", "ITEM_CRUSHED_CLAW", "ITEM_CRUSHED_CLAW", 16),
+	Metadata.new("has_injector", INJECTOR, "ITEM_INJECTOR", 17),
+	Metadata.new("resize_injector", "ITEM_INJECTOR_XL_NAME", "ITEM_INJECTOR_XL", 18),
 ]
 static var foods: Array[Metadata] = all_items_metadata.slice(0, 8)
 static var portal_cores: Array[Metadata] = all_items_metadata.slice(14, 20)
 var held_items: Array[Button] = []
-@export var descriptions: Translation
 
 func _ready() -> void:
 	for item in all_items_metadata:
@@ -106,9 +105,9 @@ func add_item(metadata: Metadata) -> void:
 func describe_item(metadata: Metadata) -> void:
 	$VBoxContainer/ItemName.text = metadata.title
 	if metadata.title == PYRITE:
-		$VBoxContainer/Description.text = descriptions.get_message(metadata.description_translation) % (Global.flags.get("pyrite", 0) as int)
+		$VBoxContainer/Description.text = tr(metadata.description_translation) % (Global.flags.get("pyrite", 0) as int)
 	else:
-		$VBoxContainer/Description.text = descriptions.get_message(metadata.description_translation)
+		$VBoxContainer/Description.text = tr(metadata.description_translation)
 
 static func get_unused_food() -> Metadata:
 	return get_unused_item(foods)
@@ -125,4 +124,4 @@ static func get_unused_item(item_set: Array[Metadata]) -> Metadata:
 	return null
 
 static func used_flag(metadata: Metadata) -> String:
-	return metadata.description_translation + "_USED"
+	return metadata.description_translation.split("_", true, 1)[1] + "_USED"
